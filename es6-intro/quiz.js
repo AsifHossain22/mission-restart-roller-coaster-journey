9cdd59a5b9bc8a928b17d6d4693702b5b8e7c10b
AsifHossain22 <only1asif4you22@gmail.com>
2026-02-02 12:55:38 +0400

    Quiz - 1 Done

---
+++ es6-intro/quiz.js
@@ -0,0 +1,17 @@
+// Quiz 1-8
+const obj = { foo: 1 };
+obj.bar = 2;
+
+console.log(obj);
+
+// Quiz 1-9
+let a = 12;
+let b = 3;
+
+[a, b] = [b, a];
+
+const result = [a, b];
+console.log(result);
+
+// Quiz - 1 : Result - 10/10
+// Alhamdulillah!
