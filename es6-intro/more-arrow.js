5b5620bec2cd26e274d2e6abfd571f760c241184
AsifHossain22 <only1asif4you22@gmail.com>
2026-02-02 10:07:29 +0400

    Recap more about Arrow Function

---
+++ es6-intro/more-arrow.js
@@ -0,0 +1,9 @@
+// SingleParameter
+const getSquare = (parameter) => parameter * parameter;
+// console.log(getSquare(5));
+
+const getHalf = (parameter) => parameter / 2;
+// console.log(getHalf(40));
+
+// AnonymousFuntion
+document.getElementById("").addEventListener("click", () => {});
