f912a8e06c373f0fef2f33497198353d168cd391
AsifHossain22 <only1asif4you22@gmail.com>
2026-02-11 09:07:35 +0400

    Added Level Button Active Feature

--- English Janala Project/english-janala-js-api-project/script/index.js
+++ English Janala Project/english-janala-js-api-project/script/index.js
@@ -5,6 +5,13 @@ const loadLessons = () => {
     .then((data) => displayLesson(data.data));
 };
 
+const removeActiveClass = () => {
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+  //   console.log(lessonButtons);
+
+  lessonButtons.forEach((btn) => btn.classList.remove("active"));
+};
+
 // LoadWordsFunction
 const loadLevelWord = (id) => {
   //   console.log(id);
@@ -14,7 +21,13 @@ const loadLevelWord = (id) => {
 
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayLevelWord(data.data));
+    .then((data) => {
+      const clickedBtn = document.getElementById(`lesson-btn-${id}`);
+      removeActiveClass(); // RemoveAllActiveClass
+      //   console.log(clickedBtn);
+      clickedBtn.classList.add("active"); // AddActiveClass
+      displayLevelWord(data.data);
+    });
 };
 
 // DisplayLevelWordsFunction
@@ -25,26 +38,32 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla">
+        <img class="mx-auto" src="./assets/alert-error.png" />
+        <p class="text-xl font-medium text-gray-400">
+            এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+    </div>
+    `;
+
+    return;
+  }
+
   // 2. GetWords
   words.forEach((word) => {
     // console.log(word);
 
     // 3. CreateElement
 
-    // {
-    // "id": 4,
-    // "level": 5,
-    // "word": "Diligent",
-    // "meaning": "পরিশ্রমী",
-    // "pronunciation": "ডিলিজেন্ট"
-    // }
-
     const card = document.createElement("div");
     card.innerHTML = `
         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "No word found!"}</h2>
             <p class="font-semibold">Meaning / Pronounciation</p>
-            <div class="font-bangla font-medium text-2xl">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "No meaning found"} / ${word.pronunciation ? word.pronunciation : "No pronounciation found!"}"</div>
             <div class="flex justify-between items-center">
               <button
                 class="cursor-pointer bg-[#1A91FF10] hover:bg-[#1A91FF80] transition-colors duration-300 p-3"
@@ -76,7 +95,7 @@ const displayLesson = (lessons) => {
     // 3. CreateElement
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+        <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
             <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
         </button>
     `;
