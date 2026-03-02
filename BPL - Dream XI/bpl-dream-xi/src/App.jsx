05ab3f67009d538a1c6f0b7f78020409c35092ea
AsifHossain22 <only1asif4you22@gmail.com>
2026-03-02 13:37:44 +0400

    Created Navbar

--- BPL - Dream XI/bpl-dream-xi/src/App.jsx
+++ BPL - Dream XI/bpl-dream-xi/src/App.jsx
@@ -1,10 +1,138 @@
-import "./App.css";
+import React from "react";
+import logo from "./assets/images/logo.png";
+import { TbCoinFilled } from "react-icons/tb";
 
 function App() {
   return (
     <>
       {/* Header */}
-      <header></header>
+      <header className="bg-base-100 shadow-sm">
+        {/* Navbar */}
+        <div className="navbar justify-between max-w-7xl mx-auto px-4 lg:px-0">
+          <div className="navbar-start">
+            {/* Logo */}
+            <a
+              href="#"
+              className="hover:scale-110 transition-transform duration-300"
+            >
+              <img src={logo} alt="BPL Dream XI Logo" className="w-16" />
+            </a>
+          </div>
+          {/* DesktopNav */}
+          <div className="navbar-end hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 gap-12 items-center">
+              <li>
+                <a
+                  href="#"
+                  className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#fixture"
+                  className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0"
+                >
+                  Fixture
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#teams"
+                  className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0"
+                >
+                  Teams
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#schedules"
+                  className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0"
+                >
+                  Schedules
+                </a>
+              </li>
+
+              {/* CoinsDisplayButton */}
+              <div className="">
+                <a className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0">
+                  0 Coin <TbCoinFilled />
+                </a>
+              </div>
+            </ul>
+          </div>
+
+          {/* MobileNav */}
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden p-0"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow right-0"
+            >
+              <li>
+                <a
+                  href="#"
+                  className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#fixture"
+                  className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0"
+                >
+                  Fixture
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#teams"
+                  className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0"
+                >
+                  Teams
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#schedules"
+                  className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0"
+                >
+                  Schedules
+                </a>
+              </li>
+
+              {/* CoinsDisplayButton */}
+              <div className="text-center">
+                <a className="btn font-sora bg-transparent shadow-none border-none text-nav-link hover:text-hover-nav-link hover:bg-transparent hover:shadow-none transition-all duration-300 p-0">
+                  0 Coin <TbCoinFilled />
+                </a>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </header>
 
       {/* Main */}
       <main></main>
